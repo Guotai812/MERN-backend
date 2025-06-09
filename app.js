427c93bc370df23379d9f0506def43d53c8276c0
guotai <guotaixiao87@gmail.com>
2025-06-09 12:01:36 +1000

    feat: add not found page midddle ware

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const placesRouter = require("./routes/places-route");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use("/api/places", placesRouter);
+
+app.use((req, res, next) => {
+    throw new HttpError("Could not find page", 404);
+});
 
 app.use((error, req, res, next) => {
      if(res.headerSent) {
