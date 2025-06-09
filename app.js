1261a5ee3877b2b75194bd5486517e6675b5219d
guotai <guotaixiao87@gmail.com>
2025-06-09 11:04:19 +1000

    feat: add centralized error-handling middleware

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const placesRouter = require("./routes/places-route");
 
 const app = express();
 
-app.use(placesRouter);
+app.use("/api/places", placesRouter);
+
+app.use((error, req, res, next) => {
+     if(res.headerSent) {
+        return next(error);
+     }
+     res.status( error.code || 500);
+     res.json({message: error.message || "An unknown error occured!"});
+});
 
 app.listen(5002);
 
--- routes/places-route.js
+++ routes/places-route.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const HttpError = require("../models/http-error");
 
-router.get("/", (req, res, next) => {
-    console.log("places-route");
-    res.json({message: "asd"});
-})
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
+];
+
+router.get("/:pid", (req, res, next) => {
+    const pid = req.params.pid;
+    const place = DUMMY_PLACES.find(place => place.id == pid)
+    if(!place) {
+      throw new HttpError("Counld not find place", 404);
+    }
+    res.json(place);
+});
+
+router.get("/users/:uid", (req, res, next) => {
+    const uid = req.params.uid;
+    const places = DUMMY_PLACES.filter(p => p.creator == uid);
+    if(places.length === 0) {
+      throw new HttpError("could not find palces", 404);
+    }
+    res.json(places);
+});
 
 module.exports = router;
